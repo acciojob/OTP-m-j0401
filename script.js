c96189b0b888d50418b524467bc145bd77408347
m-j0401 <mehul.jain2206@gmail.com>
2026-02-03 14:01:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+let input=document.querySelectorAll(".code");
+for(let i of input)
+{
+i.addEventListener("input",verify)
+}
+function verify(e) {
+	let t=e.target;
+	if(t<input.length-1)
+	{
+	   t+1.addClassList("selected");
+		
+	}
+	
+}
+
+
